00414d56587e21962bcabe365f2cccab9fd3b1e9
Oneiber Quintero <oneiber.37.37@gmail.com>
2023-07-19 19:01:35 -0400

    fix improved file size

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,5 @@
-// import Home from './Home'
-// import About from './About'
-// import Menu from './Menu'
 import useSelectedMenu from '../hooks/useSelectedMenu'
-// import Courses from './Courses'
-// import Academies from './Academies'
-// import Experiences from './Experiences'
-// import WebSites from './WebSites'
-import { Suspense, lazy } from 'react'
+import { lazy } from 'react'
 const Home = lazy(() => import('./Home'))
 const About = lazy(() => import('./About'))
 const Menu = lazy(() => import('./Menu'))
@@ -26,14 +19,12 @@ const Main = ({ info }) => {
     <>
       <div className="w-full sm:w-8/12 md:w-7/12  sm:h-full sm:rounded-r-lg p-2 bg-bgTertiary flex justify-between items-start">
         <div className="h-full w-full p-4 hidden sm:block">
-          <Suspense>
           { isMenu('home') && selectedOption === 'home' && <Home key="mHome" info={info} />}
           { isMenu('about') && selectedOption === 'about' && <About key="mAbout" info={info} />}
           { isMenu('academies') && selectedOption === 'academies' && <Academies key="mAcademies" info={info} />}
           { isMenu('experiences') && selectedOption === 'experiences' && <Experiences key="mExperiences" info={info} />}
           { isMenu('courses') && selectedOption === 'courses' && <Courses key="mCourses" info={info} />}
           { isMenu('websites') && selectedOption === 'websites' && <WebSites key="mWebSites" info={info} />}
-          </Suspense>
 
         </div>
         <div className="h-full w-full p-4 block sm:hidden">
@@ -44,7 +35,9 @@ const Main = ({ info }) => {
           { isMenu('courses') && <Courses key="mrCourses" info={info} />}
           { isMenu('websites') && <WebSites key="mrWebSites" info={info} />}
         </div>
-        <Menu info={info} setSelectedOption={setSelectedOption} />
+
+          <Menu info={info} setSelectedOption={setSelectedOption} />
+
       </div>
     </>
   )
